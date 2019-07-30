989c343947bc9d4886ce6a1c9c9d49e03d35c814
jborza <jborza@gmail.com>
2019-07-30 16:59:29 +0200

    opcode functions generator

---
+++ gen/gen_cpu_opcodes.py
@@ -0,0 +1,14 @@
+with open('rv32i.txt','r') as f:
+    lines = [line.rstrip('\n') for line in f.readlines()]
+    #print(lines)
+    for line in lines:
+        print('void %s(State* state, word* instruction) {' % line)
+        print('printf("%s not implemented!\\n"); exit(1);' % line)
+        print('}')
+
+
+    for line in lines:
+        #print(line)
+        print('else if ((*instruction & MASK_%s) == MATCH_%s) {' % (line.upper(), line.upper()))
+        print('     %s(state, instruction);' % line)
+        print(')})')
