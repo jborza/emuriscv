3037e72d64f8cc6bd1033d3f2b7492ed47b458e5
jborza <jborza@gmail.com>
2019-08-28 20:06:04 +0200

    sign extended address for I-type immediate

--- decode.c
+++ decode.c
@@ -20,7 +20,12 @@ int32_t get_b_imm(word value) {
 }
 
 int32_t get_i_imm(word value) {
-	return bextr(value, 20, 12);
+	int val = bextr(value, 20, 12);
+	int sign = bextr(value, 31, 1) == 1 ? -1 : 1;
+	//sign extend if negative
+	if (sign == -1)
+		val |= (0xfffff000);
+	return val;
 }
 
 int32_t get_s_imm(word value) { 
