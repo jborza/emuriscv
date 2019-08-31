1a047547461b60af7e1c36e3a989b32ad83b96c8
jborza <jborza@gmail.com>
2019-08-31 19:21:50 +0200

    fixed lw/sw test suite to be more resilient

--- test/lw_sw_offset.s
+++ test/lw_sw_offset.s
@@ -6,10 +6,12 @@ sw x1, (x3)
 lw x30, (x3)
 bne x30, x1, failure
 test2: #positive offset
+li x1, 0x99887766
 sw x1, 8 (x3)
 lw x30, 8 (x3)
 bne x30, x1, failure
 test3: #large positive offset
+li x1, 0x66778899
 sw x1, 2044(x3)
 lw x30, 2044(x3)
 bne x30, x1, failure
@@ -18,13 +20,13 @@ li x3, 0x2004
 li x1, 0xdeadbeef
 sw x1, -4(x3)
 lw x30, -4(x3)
-li x31, 0xdeadbeef
-bne x30, x31, failure
+bne x30, x1, failure
 test5: #large negative offset
+li x1, 0xabcddcba
 li x3, 0x4000
 sw x1, -996(x3)
 lw x30, -996(x3)
-beq x30, x31, success
+beq x30, x1, success
 failure:	
 	li a0, 0
 	li a7, 93
