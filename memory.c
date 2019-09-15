f15fef142d47068b1fc41969f54c349dd0d4e9e5
jborza <jborza@gmail.com>
2019-09-15 13:01:23 +0200

    memory reads allowing redirect to memory_mapped I/O

--- memory.c
+++ memory.c
@@ -33,7 +33,7 @@ word * fetch_next_word(State * state) {
 	return address;
 }
 
-void write_common_ram(State* state, uint8_t *target, word value, int size_log2) {
+void write_common_ram(State * state, uint8_t * target, word value, int size_log2) {
 	switch (size_log2) {
 	case SIZE_WORD:
 		*(target + 3) = (value & 0xff000000) >> 24;
@@ -44,36 +44,47 @@ void write_common_ram(State* state, uint8_t *target, word value, int size_log2) 
 		*target = value & 0xff;
 		break;
 	default:
-		fprintf(stderr, "write_common unsupported size:%d", size_log2);
+		fprintf(stderr, "write_common_ram unsupported size:%d", size_log2);
 		break;
 	}
 }
 
-void write_common(State* state, word address, word value, int size_log2) {
-	uint8_t* ptr;
+typedef struct MemoryTarget {
 	MemoryRange* range;
-	uint32_t target_address = address;
-	range = get_phys_mem_range(state->memory_map, target_address);
+	uint8_t* ptr;
+} MemoryTarget;
 
-	if (!range) {
+MemoryTarget get_memory_target(State * state, word address) {
+	MemoryTarget target;
+	uint8_t* ptr;
+	uint32_t target_address = address;
+	target.range = get_phys_mem_range(state->memory_map, target_address);
+
+	if (!target.range) {
 		printf("get_physical_address: invalid physical address 0x%08x\n", target_address);
-		return 0;
+		exit(1);
+		return target;
 	}
-	ptr = range->phys_mem_ptr + (uint32_t)(target_address - range->address);
-	if (range->is_ram) {
-		write_common_ram(state, ptr, value, size_log2);
+	target.ptr = target.range->phys_mem_ptr + (uint32_t)(target_address - target.range->address);
+	return target;
+}
+
+void write_common(State * state, word address, word value, int size_log2) {
+	MemoryTarget target = get_memory_target(state, address);
+	if (target.range->is_ram) {
+		write_common_ram(state, target.ptr, value, size_log2);
 	}
 	else {
 		//device I/O
-		range->write_func(range->opaque, ptr, value, size_log2);
+		target.range->write_func(target.range->opaque, target.ptr, value, size_log2);
 	}
 }
 
-void write_word(State* state, word address, word value) {
+void write_word(State * state, word address, word value) {
 	write_common(state, address, value, SIZE_WORD);
 }
 
-void write_halfword(State* state, word address, halfword value) {
+void write_halfword(State * state, word address, halfword value) {
 	write_common(state, address, value, SIZE_HALF);
 }
 
@@ -81,40 +92,48 @@ void write_byte(State * state, word address, byte value) {
 	write_common(state, address, value, SIZE_BYTE);
 }
 
-//TODO is this little-endian?
-//void write_word(State * state, word address, word value) {
-//	uint8_t* target = get_physical_address(state, address);
-//	*target = value & 0xff;
-//	*(target + 1) = (value & 0xff00) >> 8;
-//	*(target + 2) = (value & 0xff0000) >> 16;
-//	*(target + 3) = (value & 0xff000000) >> 24;
-//}
-//
-//void write_halfword(State * state, word address, halfword value) {
-//	uint8_t* target = get_physical_address(state, address);
-//	*target = value & 0xff;
-//	*(target + 1) = (value & 0xff00) >> 8;
-//}
-//
-//void write_byte(State * state, word address, byte value) {
-//	uint8_t* target = get_physical_address(state, address);
-//	*target = value & 0xff;
-//}
+// READ 
 
-//TODO is this little-endian?
-word read_word(State * state, word address) {
-	uint8_t* target = get_physical_address(state, address);
-	word value = *target
-		| *(target + 1) << 8
-		| *(target + 2) << 16
-		| *(target + 3) << 24;
+word read_common_ram(State * state, uint8_t * target, int size_log2) {
+	word value;
+	switch (size_log2) {
+	case SIZE_WORD:
+		value = *target
+			| *(target + 1) << 8
+			| *(target + 2) << 16
+			| *(target + 3) << 24;
+		break;
+	case SIZE_HALF:
+		value = *target | *(target + 1) << 8;
+		break;
+	case SIZE_BYTE:
+		value = *target;
+		break;
+	default:
+		fprintf(stderr, "read_common_ram unsupported size:%d", size_log2);
+		exit(1);
+		return 0;
+	}
 	return value;
 }
 
-word read_halfword_signed(State * state, word address) {
-	uint8_t* target = get_physical_address(state, address);
-	word value = *target | *(target + 1) << 8;
+word read_common(State * state, word address, int size_log2) {
+	MemoryTarget target = get_memory_target(state, address);
+	if (target.range->is_ram) {
+		return read_common_ram(state, target.ptr, size_log2);
+	}
+	else {
+		//device I/O
+		return target.range->read_func(target.range->opaque, target.ptr, size_log2);
+	}
+}
 
+word read_word(State * state, word address) {
+	return read_common(state, address, SIZE_WORD);
+}
+
+word read_halfword_signed(State * state, word address) {
+	word value = read_common(state, address, SIZE_HALF);
 	//sign extend 16-bit value
 	if ((value & 0x8000) == 0x8000)
 		return value | 0xffff0000;
@@ -123,16 +142,12 @@ word read_halfword_signed(State * state, word address) {
 }
 
 word read_halfword_unsigned(State * state, word address) {
-	uint8_t* target = get_physical_address(state, address);
-	word value = *target | *(target + 1) << 8;
-
+	word value = read_common(state, address, SIZE_HALF);
 	return value;
 }
 
-
 word read_byte_signed(State * state, word address) {
-	uint8_t* target = get_physical_address(state, address);
-	word value = *target;
+	word value = read_common(state, address, SIZE_BYTE);
 	//sign extend 8-bit value
 	if ((value & 0x80) == 0x80)
 		return value | 0xffffff00;
@@ -141,7 +156,6 @@ word read_byte_signed(State * state, word address) {
 }
 
 word read_byte_unsigned(State * state, word address) {
-	uint8_t* target = get_physical_address(state, address);
-	word value = *target;
+	word value = read_common(state, address, SIZE_BYTE);
 	return value;
 }
