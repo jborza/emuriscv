0db7f3a0200ccb7478e56b868e7143bec803c75a
jborza <jborza@gmail.com>
2021-04-11 23:10:42 +0200

    sifive uart header

---
+++ uart.h
@@ -0,0 +1,11 @@
+#pragma once
+#define UART_REG_TXFIFO		0
+#define UART_REG_RXFIFO		1
+#define UART_REG_TXCTRL		2
+#define UART_REG_RXCTRL		3
+#define UART_REG_IE			4
+#define UART_REG_IP			5
+#define UART_REG_DIV			6
+
+#define UART_TXEN		 0x1
+#define UART_RXEN		 0x1
