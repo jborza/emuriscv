5cde52b8de91e25ffef7e922fe4d36013ecab528
jborza <jborza@gmail.com>
2021-04-15 19:00:42 +0200

    windows ebreak implementation -> __debugbreak()

--- cpu_ebreak.c
+++ cpu_ebreak.c
@@ -1,10 +1,15 @@
 #include "cpu_ebreak.h"
 #include "debug.h"
 #include <stdio.h>
+#include <intrin.h>
 
 void ebreak(State* state, word* instruction) {
 	PRINT_DEBUG("ebreak\n");
 #ifdef _DEBUG
+#ifdef _WIN32
+	__debugbreak();
+#elif
 	raise(SIGINT); //TODO or other reliable "invoke debugger" call
+#endif
 #endif
 }
